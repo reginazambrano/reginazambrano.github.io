6c877950ba758dc90338c3f62e4abab9ed93b74e
Regina Zambrano <rzambra1@nd.edu>
2019-03-09 21:51:35 -0600

    project2

---
+++ project2/js/popup-template.js
@@ -0,0 +1,42 @@
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#quickstart-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.    
+         $(".modal-content").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#newgame').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.    
+         $(".modal-content").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#howplay').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.    
+         $(".modal-content2").toggleClass("visible");
+        });
+});
+
+
+//************************ Templates ************************
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#place-button-name-here').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#place-modal-name-here").toggleClass("visible");
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
